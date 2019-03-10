65746487fce1bdbfba62516e08ec8eee64e03d05
qqavon <qqavon@gmail.com>
2019-03-10 19:44:26 +0100

    Included categories component to home view

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,8 +1,22 @@
 <template>
   <div class="home">
-    <h1> Strona główna </h1>
+    <h1> Kategorie ogłoszeń </h1>
+    <home-categories></home-categories>
   </div>
 </template>
 
 <script>
+import HomeCategories from '../components/HomeCategories.vue'
+
+export default {
+  components: {
+    'home-categories': HomeCategories
+  }  
+}
 </script>
+
+<style scoped>
+h1 {
+  text-align: center;
+}
+</style>
