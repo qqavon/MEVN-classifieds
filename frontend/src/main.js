fc3a55dc30f41845584d4a9faddcc523442f7192
qqavon <qqavon@gmail.com>
2019-03-10 17:28:58 +0100

    Installed axios plugin

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios';
+
+const axiosInstance = axios.create({
+  baseURL: 'http://192.168.1.3/7777/api'
+})
+
+Vue.use(VueAxios, axiosInstance)
 
 Vue.config.productionTip = false
 
